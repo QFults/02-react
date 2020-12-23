c6861f889309a57aaeef937c11f28e949a204305
Quinton Fults <quinton.fults.work@gmail.com>
2020-12-23 10:53:58 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-const App = () => {
-  return (
-    <h1>Hello World!</h1>
-  )
+import { Component } from 'react'
+
+class App extends Component {
+
+  state = {
+    count: 0
+  }
+
+  handleIncrementCount = () => {
+    this.setState({ count: this.state.count + 1})
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleIncrementCount}>Click Me</button>
+      </div>
+    )
+  }
 }
 
 export default App
